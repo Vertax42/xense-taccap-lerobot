200643592176c14bf5f9d8ab6c1f55f39b8e2a10
lewis <wufan@xenserobotics.com>
2026-09-04 15:02:11 +0800

    fix(xense): stop reader before camera release
    xensesdk can close its OpenCV backend while its internal fetch thread
    is still reading, causing intermittent segmentation faults on shutdown.
    Suspend the stream before release and lock the ordering in a regression
    test.

---
+++ tests/cameras/test_xense.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+# Copyright 2024 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from lerobot.cameras.xense.camera_xense import XenseTactileCamera
+
+
+def test_disconnect_suspends_sdk_stream_before_release():
+    calls = []
+
+    class FakeSDKCamera:
+        def suspend_stream(self):
+            calls.append("suspend")
+
+    class FakeContext:
+        def get_handle(self, key):
+            assert key == "cam"
+            return FakeSDKCamera()
+
+    class FakeSensor:
+        ctx = FakeContext()
+
+        def release(self):
+            calls.append("release")
+
+    camera = object.__new__(XenseTactileCamera)
+    camera.serial_number = "GSPS01A29Z0021"
+    camera.sensor = FakeSensor()
+    camera.thread = None
+
+    camera.disconnect()
+
+    assert calls == ["suspend", "release"]
+    assert camera.sensor is None
